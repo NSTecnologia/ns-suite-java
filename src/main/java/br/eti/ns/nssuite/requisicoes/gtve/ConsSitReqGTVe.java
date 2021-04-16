3c616a724811bd1c4ec2b84a4bb181529814a2b0
Fernando Konflanz <konflanz.info@gmail.com>
2021-04-16 18:23:07 -0300

    Update GTVe

---
+++ src/main/java/br/eti/ns/nssuite/requisicoes/gtve/ConsSitReqGTVe.java
@@ -0,0 +1,7 @@
+package br.eti.ns.nssuite.requisicoes.gtve;
+
+import br.eti.ns.nssuite.requisicoes._genericos.ConsSitReq;
+
+public class ConsSitReqGTVe extends ConsSitReq {
+    public String chGTVe;
+}
---
+++ src/main/java/br/eti/ns/nssuite/requisicoes/gtve/DownloadEventoReqGTVe.java
@@ -0,0 +1,7 @@
+package br.eti.ns.nssuite.requisicoes.gtve;
+
+import br.eti.ns.nssuite.requisicoes._genericos.DownloadEventoReq;
+
+public class DownloadEventoReqGTVe extends DownloadEventoReq {
+    public String chGTVe;
+}
